0e007ad9dc53c50124e72610c8220f50d18e4781
zabir-nabil <zabiralnazi@codeassign.com>
2018-12-01 20:27:19 +0600

    rasp-face

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # rasp-face
 Face Detection using CNN on Raspberry-PI
+
+`Training Loss: 0.0021, Accuray: 99.95% (after 30 epochs)`
+`Validation Loss: 0.0452, Accuray: 99.37% (after 30 epochs)`
+`Test Loss: 4.53, Accuracy: 71.64% (overfitted version 1)`
+
+
+# Instructions
+
+1. Download the trained model https://drive.google.com/open?id=1KO7RXmEGuMTMZpBZrVv_5865kyx_ux20
+2. Run script rasp_test.py
+3. To re-train the model with more classes -> raspberry-pi-face-detection.ipynb
---
+++ rasp_test.py
@@ -0,0 +1,46 @@
+import picamera
+from time import sleep
+from PIL import Image
+
+# confidence check with same class for continuous 3 frames
+v1 = 0, v2 = 0, v3 = 0
+
+print('--------------------------------------------------\n')
+xx = input('The program will run for X frames, Enter X (Example 100): ')
+xx = int(xx)
+for fr in range(xx):
+
+  camera = picamera.PiCamera()
+
+  camera.resolution = (512,512)
+
+  camera.start_preview()
+
+  sleep(2)
+
+  camera.capture('cur_frame.jpg')
+
+  camera.stop_preview()
+
+  x = 'cur_frame.jpg'
+  im = Image.open(x2)
+  im = im.resize((224,224), Image.ANTIALIAS)
+  a = np.array(im, dtype='float32')
+  a = np.reshape(a, (1,224,224,3))
+  a = a/255.
+  pr = model1.predict_classes(a)
+  pr = pr[0]
+  
+  v1 = v2
+  v2 = v3
+  v3 = pr
+  
+  if(v1==1 and v2==1 and v2==2):
+    print('Imran (Class 1) seen on camera')
+    print('\n')
+  elif(v1==1 and v2==1 and v2==2):
+    print('Sadman (Class 2)seen on camera')
+    print('\n')
+  else:
+    print('Background or Unknown face!')
+    print('\n')
